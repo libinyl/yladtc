a969a74d256f9b568fc2450da2e402c22afa57ec
libinyl <1102109100@qq.com>
2019-05-05 02:02:40 +0800

    minor change.

--- include/def.h
+++ include/def.h
@@ -1,8 +1,8 @@
 #ifndef DEF_H
 #define DEF_H
 
-#define RT_SUCCEED  (0) // return value for succeed process
-#define RT_FAILED  (-1) // return value for failed process
+#define RT_SUCCEED  (0)     // return value for succeed process
+#define RT_FAILED   (-1)    // return value for failed process
 
 #define TRUE    (1);
 #define FALSE   (0);
