0fd21cead8344299172356141eac06900ad4297d
libinyl <1102109100@qq.com>
2019-05-04 14:39:40 +0800

    minor fix.

--- include/list.h
+++ include/list.h
@@ -1,13 +1,15 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include "def.h"
+
 typedef struct _node
 {
-    VAL data;
-    struct _node *next;
-} node_y, *list_y;
+    VAL             data;
+    struct _node    *next;
+} node_y, list_y;
 
-int list_init(list_y list);
+int list_init(list_y *list);
 
 
 #endif //LIST_H
